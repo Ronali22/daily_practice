aeb16753265e2e5bd25347fb0c967e99965a387e
Ronali22 <roanli.panda99@gmail.com>
2025-01-19 01:49:09 +0530

    stream examples added

--- OOPsConceptRevision/src/com/InterviewPractice/revision/Example1.java
+++ OOPsConceptRevision/src/com/InterviewPractice/revision/Example1.java
@@ -1,5 +1,6 @@
 package com.InterviewPractice.revision;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,7 +9,10 @@ import java.util.stream.Collectors;
 //3. WAP to filter employees whose salary is more than 20,000
 //4. WAP to filter employees whose name starts with R
 //5. WAP to filter employees whose salary is between 20000 to 40000
-
+//6. WAP to count employees whose name starts with R
+//7. WAP to find whose salary is maximum
+//8. WAP to find whose salary is minimum
+//9. WAP to sort the employees according to their salary in descending order
 class Employee{
 	private String employeeName;
 	private String employeeGender;
@@ -60,19 +64,47 @@ Employee employee6 = new Employee("Rahul", "Male", 70000.00);
 List<Employee> employeeList= List.of(employee1, employee2, employee3, employee4, employee5, employee6);
 
 	//1.
-	List<Employee> result = employeeList.stream().filter(e->e.getEmployeeGender()=="Female").collect(Collectors.toList());
-	System.out.println(result);
+	//List<Employee> result = employeeList.stream().filter(e->e.getEmployeeGender()=="Female").collect(Collectors.toList());
+	//System.out.println(result);
 	
 	//2.
-	employeeList.stream().map(e->e.getEmployeeSalary()+20000.00).collect(Collectors.toList()).forEach(System.out::println);
+	//employeeList.stream().map(e->e.getEmployeeSalary()+20000.00).collect(Collectors.toList()).forEach(System.out::println);
 	
 	//3.
-	employeeList.stream().filter(e->e.getEmployeeSalary()>20000.00).collect(Collectors.toList()).forEach(System.out::println);
+	//employeeList.stream().filter(e->e.getEmployeeSalary()>20000.00).collect(Collectors.toList()).forEach(System.out::println);
 	
 	//4.
-	//5.
-	}
+
+//	employeeList.stream()
+//			.filter(e->e.getEmployeeName().charAt(0)=='R')
+//			.collect(Collectors.toList())
+//			.forEach(System.out::println);;
 	
-
-
+	//5.
+	
+//	List<Employee> result = employeeList.stream()
+//			.filter(e->(e.getEmployeeSalary()>20000.00 && e.getEmployeeSalary()<40000.00))
+//			.collect(Collectors.toList());
+	//System.out.println(result);
+	
+	//6.
+//	long count  = employeeList.stream()
+//			.filter(e->e.getEmployeeName().charAt(0)=='R')
+//			.count();
+	//System.out.println(count);
+	
+	//7. 
+	//Employee employeeWithMaxSalary = employeeList.stream().max((a,b)->(int)(a.getEmployeeSalary()-b.getEmployeeSalary())).get();
+	//System.out.println(employeeWithMaxSalary);
+	
+	//8. 
+	//Employee employeeWithMinSalary = employeeList.stream().min((a,b)->(int)(a.getEmployeeSalary()-b.getEmployeeSalary())).get();
+	//System.out.println(employeeWithMinSalary);
+	
+	//9.
+	List<Employee> result = employeeList.stream()
+			.sorted(Comparator.comparing(Employee::getEmployeeSalary).reversed())
+			.collect(Collectors.toList());
+	result.forEach(System.out::println);
+	}
 }
